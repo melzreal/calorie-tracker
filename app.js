3e59ee152fec956b247a5de47df79f1de9d350c4
melzreal <melissa.real@hotmail.com>
2020-03-19 17:06:50 +0000

    adding items

--- app.js
+++ app.js
@@ -12,31 +12,50 @@ const ItemCtrl = (function () {
 
     const state = {
         items: [
-            {
-                id: 0,
-                name: 'Spinach Quiche',
-                calories: 230
-            },
-            {
-                id: 1,
-                name: 'Beefy Burguer',
-                calories: 800
-            },
-            {
-                id: 2,
-                name: 'Chicken Burguer',
-                calories: 700
-            }],
+            // {
+            //     id: 0,
+            //     name: 'Spinach Quiche',
+            //     calories: 230
+            // },
+            // {
+            //     id: 1,
+            //     name: 'Beefy Burguer',
+            //     calories: 800
+            // },
+            // {
+            //     id: 2,
+            //     name: 'Chicken Burguer',
+            //     calories: 700
+            // }
+        ],
         currentItem: null,
         totalCalories: 0
     }
-
+    //PUBLIC methods
     return {
         getItems: function () {
             return state.items;
         },
         logState: function () {
             return state;
+        },
+        addItem: function (name, calories) {
+            let ID;
+
+            if (state.items.length > 0) {
+                ID = state.items[state.items.length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            //parse calories to a nr
+            //create item
+            //update state
+            calories = parseInt(calories);
+            newItem = new Item(ID, name, calories);
+            state.items.push(newItem);
+
+            return newItem;
         }
     }
 
@@ -44,6 +63,13 @@ const ItemCtrl = (function () {
 
 //our ui controller will populate our list 
 const UICtrl = (function () {
+    //if the id gets changed, its easier to change it in one place hence this cont 
+    const UISelectors = {
+        itemList: '#item-list',
+        addBtn: '.add-btn',
+        itemName: 'item-name',
+        itemCalories: 'item-calories',
+    }
     return {
         populateItems: function (goodies) {
 
@@ -59,7 +85,43 @@ const UICtrl = (function () {
             });
 
             //put items into the DOM
-            document.getElementById('item-list').innerHTML = html;
+            document.querySelector(UISelectors.itemList).innerHTML = html;
+        },
+        //this is how we access the private method of const UISelectos
+        getSelectors: function () {
+            return UISelectors;
+        },
+
+        getItemInput: function () {
+            return {
+                name: document.getElementById(UISelectors.itemName).value,
+                calories: document.getElementById(UISelectors.itemCalories).value
+            }
+        },
+        addListItem: function (item) {
+            document.querySelector(UISelectors.itemList).style.display = 'block';
+
+            //create li element
+            //add class - col item is a materialize class
+            const li = document.createElement('li');
+            li.className = 'collection-item';
+            li.id = `item-${item.id}`;
+            li.innerHTML = `<strong> ${item.name} </strong> <em> ${item.calories}</em>
+            <a href="#" class="secondary-content">
+                <i class="edit-item fa fa-pencil"></i>
+            </a>`;
+
+            //insert it
+            document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li)
+
+
+        },
+        hideList: function () {
+            document.querySelector(UISelectors.itemList).style.display = 'none';
+        },
+        clearInput: function () {
+            document.getElementById(UISelectors.itemName).value = '';
+            document.getElementById(UISelectors.itemCalories).value = ''
         }
     }
 
@@ -67,12 +129,39 @@ const UICtrl = (function () {
 
 const App = (function (ItemCtrl, UICtrl) {
 
+    const loadEventListener = function () {
+        const UISelectors = UICtrl.getSelectors();
+        document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+    }
+    //we need to call this on our init function
+    const itemAddSubmit = function (e) {
+        e.preventDefault();
+        const input = UICtrl.getItemInput();
+
+        //check input exists
+        //we will use newItem to update the UI
+        //so we first store the item in state using the Item Controller
+        //then place it in the UI with a UI addListItem method
+        //finally clear fields
+        if (input.name !== '' && input.calories !== '') {
+            const newItem = ItemCtrl.addItem(input.name, input.calories);
+            UICtrl.addListItem(newItem);
+        }
+        UICtrl.clearInput();
+
+    }
     //anything we need to run when the app loads, we place here.
     return {
         init: function () {
             const items = ItemCtrl.getItems();
+            if (items.length === 0) {
+                UICtrl.hideList();
+            } else {
+                UICtrl.populateItems(items);
+            }
 
             UICtrl.populateItems(items);
+            loadEventListener();
         }
     }
 
